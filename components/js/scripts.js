f87837bed2560c430dcd1544ddab032c907392f1
joshboyan <joshboyan@yahoo.com>
2016-11-25 16:23:04 -0800

    Change className to classList.toggle()

--- components/js/scripts.js
+++ components/js/scripts.js
@@ -1,5 +1,6 @@
 /*You can have as many different javascript files as you like. 
 They will all be compiled to ES5, concantenated and linted for the dev build and
 minified with comments removed for the dist build.*/
+
 document.onscroll= () =>{ document.querySelector('#navbar').className='navbar visible';};
-document.querySelector('#developers').onclick = () => {document.querySelector('#submenu').className='slideUp slideDown'}
+document.querySelector('.closed').onclick = () => {document.querySelector('#submenu').classList.toggle("slideDown");};
