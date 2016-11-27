55401f05c8f286a606146758988fd86746a519f4
joshboyan <joshboyan@yahoo.com>
2016-11-26 19:33:35 -0800

    Finish styling main site nav

--- components/js/scripts.js
+++ components/js/scripts.js
@@ -3,4 +3,10 @@ They will all be compiled to ES5, concantenated and linted for the dev build and
 minified with comments removed for the dist build.*/
 
 document.onscroll= () =>{ document.querySelector('#navbar').className='navbar visible';};
-document.querySelector('.closed').onclick = () => {document.querySelector('#submenu').classList.toggle("slideDown");};
+document.querySelector('.closed').onclick = () => { document.querySelector('#submenu').classList.toggle("slideDown");};
+document.querySelector('#mobileTrigger').onclick = function() { 
+	document.querySelector('.mobileNav').classList.toggle('slideOut');
+	document.querySelector('#mobileTrigger i').classList.toggle('spinner');
+	document.querySelector('#mobileTrigger i').classList.toggle('fa-bars')
+	document.querySelector('#mobileTrigger i').classList.toggle('fa-times');
+}
